a02a6c9c2ba87495fd0d35c2f9da51b3c1f6278e
suratasbasii@gmail.com <suratasbasii@gmail.com>
2024-08-02 22:37:41 +0300

    Add project files.

---
+++ MetotUygulamalarii/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+    static void Main()    // Öncelikle Main metodu oluşturup metodları çağırıyoruz.
+    {
+        SarkiSozu();  // Geriye değer döndürmeyen metodu çağırma
+
+        int kalan = RandomSayi();  //  Geriye değer döndüren metodu çağırma
+        Console.WriteLine("Rastgele bir sayının 2Ye bölümünden kalan: " + kalan);
+
+        int carpimSonucu = Carpim(2, 8);  // Parametre alıp geriye değer döndüren metodu çağırma
+        Console.WriteLine("Çarpım: " + carpimSonucu);
+
+        IsimSoyisim("Şura", "Taşbaşı");  //  Parametre alıp geriye değer döndürmeyen metodu çağırma
+
+    }
+
+
+
+    static void SarkiSozu()
+    {
+        Console.WriteLine("Kul Ahmet erken kalkar, \"Haydi ya nasip\" derdi.\nKimseler anlamazdı \"Ya nasip\" ne demekti.");
+    }
+
+
+
+    static int RandomSayi()
+    {
+        Random rnd = new Random();
+        int sayi = rnd.Next();
+        return sayi % 2;
+    }
+
+
+
+    static int Carpim(int a, int b)
+    {
+        return a * b;
+    }
+
+
+
+    static void IsimSoyisim(string isim, string soyisim)
+    {
+        Console.WriteLine($"Hoş Geldiniz {isim} {soyisim} ");
+    }
+
+}
